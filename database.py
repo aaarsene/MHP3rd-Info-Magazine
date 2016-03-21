1ab74b8b7b9d620d9636c631abca5600923b6102
Arsène Thieffry <druz320@gmail.com>
2016-03-21 22:05:42 +0100

    Début de l'interface graphique

--- database.py
+++ database.py
@@ -1,31 +1,53 @@
 import sqlite3
 
-conn = sqlite3.connect('mhp3rd.db')
 
-c = conn.cursor()
 
-for row in c.execute("SELECT `ID`, `Name` FROM `Monster` ORDER BY `ID`") :
-    print("{}  {}".format(row[0], row[1]))
+class MHP3DB :
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        self.c = self.conn.cursor()
 
-print()
+    def get_monsters(self):
+        """
+        Return the list of the monsters
+        """
+        monsters = []
+        for row in self.c.execute("SELECT `ID`, `Name` FROM `Monster` ORDER BY `ID`") :
+            monsters.append(row)
+        return monsters
 
-parts = []
-for row in c.execute("SELECT PartID, Name \
-                      FROM Monster_Part \
-                      LEFT JOIN Body_Part \
-                      ON Body_Part.ID = Monster_Part.PartID \
-                      WHERE MonsterID = 1 \
-                      ORDER BY PartID") :
-    parts.append(row)
+    def get_parts(self, monster_id):
+        parts = []
+        for row in self.c.execute("SELECT PartID, Name \
+                                   FROM Monster_Part \
+                                   LEFT JOIN Body_Part \
+                                   ON Body_Part.ID = Monster_Part.PartID \
+                                   WHERE MonsterID = {} \
+                                   ORDER BY PartID \
+                                   ".format(monster_id)) :
+            parts.append(row)
+        return parts
 
-line = "Body Part"
-for row in c.execute("SELECT Name FROM Damage_Type") :
-    line = line + " | " + row[0]
+    def get_damages(self, monster_id):
+        damages = []
+        parts = self.get_parts(monster_id)
+        for part in parts :
+            damages.append([])
+            damages[-1].append(part[1])
+            for row in self.c.execute("SELECT `Damages` \
+                                  FROM `Damage_Resistance` \
+                                  WHERE MonsterID = {0} \
+                                  AND BodyPart = {1} \
+                                  ".format(monster_id, part[0])) :
+                damages[-1].append(row[0])
+        return damages
 
-print(line)
+    def get_damage_type(self):
+        types = []
+        for row in self.c.execute("SELECT Name FROM Damage_Type") :
+            types.append(row)
+        return types
 
-for part in parts:
-    line = part[1]
-    for row in c.execute("SELECT `Damages` FROM `Damage_Resistance` WHERE MonsterID = 1 AND BodyPart = {}".format(part[0])) :
-        line = line + " | {}".format(row[0])
-    print(line)
+if __name__ == '__main__':
+    db = MHP3DB('mhp3rd.db')
+    print(db.get_damages(1))
--- monster.py
+++ monster.py
@@ -1,14 +1,60 @@
 import gi
+from database import MHP3DB
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class MonsterWindow(Gtk.Window):
-    def __init__(self):
+class MonsterInfoWindow(Gtk.Window):
+    def __init__(self, monster):
         Gtk.Window.__init__(self, title="MHP3rd Info Magazine")
         self.set_default_size(480,360)
-        self.show_all()
+        self.db = MHP3DB('mhp3rd.db')
+        self.damage_table = self.db.get_damages(monster)
 
-win = MonsterWindow()
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+        self.liststore = Gtk.ListStore(str, int, int, int, int, int, int ,int, int)
+        for body_part in self.damage_table :
+            self.liststore.append(body_part)
+
+        treeview = Gtk.TreeView(model=self.liststore)
+
+        renderer_text = Gtk.CellRendererText()
+        colum_part = Gtk.TreeViewColumn("Body Part", renderer_text, text=0)
+        treeview.append_column(colum_part)
+
+        damage_types = self.db.get_damage_type()
+        columns = []
+        renderer_columns = []
+        for damage_type in damage_types :
+            renderer_columns.append(Gtk.CellRendererText())
+            columns.append(Gtk.TreeViewColumn(damage_type[0],
+                renderer_columns[-1], text=damage_types.index(damage_type)+1))
+            treeview.append_column(columns[-1])
+
+        self.add(treeview)
+
+class MonstersWindow(Gtk.Window):
+    def __init__(self, monsters):
+        Gtk.Window.__init__(self, title="MHP3rd Info Magazine")
+        self.set_default_size(480,360)
+        self.liststore = Gtk.ListStore(int, str)
+        for monster in monsters :
+            self.liststore.append(monster)
+
+        treeview = Gtk.TreeView(model=self.liststore)
+
+        renderer_text = Gtk.CellRendererText()
+        column_text = Gtk.TreeViewColumn("#", renderer_text, text=0)
+        treeview.append_column(column_text)
+
+        renderer_editabletext = Gtk.CellRendererText()
+        column_editabletext = Gtk.TreeViewColumn("Name",
+            renderer_editabletext, text=1)
+        treeview.append_column(column_editabletext)
+
+        self.add(treeview)
+
+
+if __name__ == '__main__':
+    win = MonsterInfoWindow(1)
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
